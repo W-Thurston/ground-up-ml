58fbec8fccebf6b77c6fbc3b48e081261195fec9
W-Thurston <WilliamD.Thurston@gmail.com>
2025-04-22 06:46:21 -0600

    Initialize MLflow

--- mini_projects/01_simple_linear_regression/from_scratch/slr.py
+++ mini_projects/01_simple_linear_regression/from_scratch/slr.py
@@ -1,1 +1,6 @@
 # slr.py placeholder
+from shared_utils.mlflow_logger import start_run, log_params, log_metrics
+
+with start_run("SLR_from_scratch"):
+    log_params({"learning_rate": 0.01, "epochs": 100})
+    log_metrics({"mse": 12.7, "r2": 0.91})
