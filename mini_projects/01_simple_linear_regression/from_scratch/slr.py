c3e252ddbcde2785ea23870b137f8172276941ff
W-Thurston <WilliamD.Thurston@gmail.com>
2025-04-23 11:33:26 -0600

    fix: pre-commit checks

--- mini_projects/01_simple_linear_regression/from_scratch/slr.py
+++ mini_projects/01_simple_linear_regression/from_scratch/slr.py
@@ -5,17 +5,18 @@ import time
 import numpy as np
 import pandas as pd
 
-from shared_utils.mlflow_logger import log_metrics, log_params, start_run
+# from shared_utils.mlflow_logger import log_metrics, log_params, start_run
 
 
-class SimpleLinearRegression():
+class SimpleLinearRegression:
     ALL_METHODS = [
-                "beta_estimations",]
-            #     "normal_equation",
-            #     "gradient_descent_batch",
-            #     "gradient_descent_stochastic",
-            #     "gradient_descent_mini_batch"
-            # ]
+        "beta_estimations",
+    ]
+    #     "normal_equation",
+    #     "gradient_descent_batch",
+    #     "gradient_descent_stochastic",
+    #     "gradient_descent_mini_batch"
+    # ]
 
     def __init__(self, x: pd.Series, y: pd.Series):
         self.x = x.to_numpy()
@@ -43,7 +44,9 @@ class SimpleLinearRegression():
         elif self.method == "gradient_descent_mini_batch":
             self._fit_gradient_descent_mini_batch()
         else:
-            raise ValueError(f"Unknown method '{self.method}' for SimpleLinearRegression.")
+            raise ValueError(
+                f"Unknown method '{self.method}' for SimpleLinearRegression."
+            )
 
     def _fit_beta_estimations(self):
         """
@@ -55,22 +58,20 @@ class SimpleLinearRegression():
         y_bar = np.mean(self.y)
 
         # Compute coefficient estimates
-        self.beta_1_hat = np.sum( (self.x - x_bar) * (self.y - y_bar) ) / np.sum( (self.x - x_bar)**2 )
+        self.beta_1_hat = np.sum((self.x - x_bar) * (self.y - y_bar)) / np.sum(
+            (self.x - x_bar) ** 2
+        )
         self.beta_0_hat = y_bar - self.beta_1_hat * x_bar
 
-    def _fit_normal_equation(self):
-        ...
-    def _fit_gradient_descent_batch(self):
-        ...
-    def _fit_gradient_descent_stochastic(self):
-        ...
-    def _fit_gradient_descent_mini_batch(self):
-        ...
+    def _fit_normal_equation(self): ...
+    def _fit_gradient_descent_batch(self): ...
+    def _fit_gradient_descent_stochastic(self): ...
+    def _fit_gradient_descent_mini_batch(self): ...
 
-    def predict(self, x_new: pd.Series=None):
+    def predict(self, x_new: pd.Series = None):
         if x_new is None:
             x_new = self.x
-        return self.beta_0_hat + self.beta_1_hat*x_new
+        return self.beta_0_hat + self.beta_1_hat * x_new
 
     def residuals(self):
         return self.y - self.predict()
@@ -85,7 +86,9 @@ class SimpleLinearRegression():
         return np.mean(np.abs(y_actual - y_pred))
 
     def _calculate_r_squared(self, y_actual, y_pred):
-        return 1 - np.sum((y_actual - y_pred)**2) / np.sum((y_actual - np.mean(y_actual))**2)
+        return 1 - np.sum((y_actual - y_pred) ** 2) / np.sum(
+            (y_actual - np.mean(y_actual)) ** 2
+        )
 
     def _format_duration(self, seconds: float) -> str:
         if seconds < 1e-3:
@@ -98,42 +101,48 @@ class SimpleLinearRegression():
             mins, secs = divmod(seconds, 60)
             return f"{int(mins)}m {secs:.2f}s"
 
+    def _coefficient_estimators(
+        self, x: pd.Series, y: pd.Series, n: int, methods: str = None
+    ):
 
-    def _coefficient_estimators(self, x: pd.Series, y: pd.Series, n: int, methods: str = None):
+        coeff_results = []
+        methods = methods or SimpleLinearRegression.ALL_METHODS
 
-            coeff_results = []
-            methods = methods or SimpleLinearRegression.ALL_METHODS
+        for method in methods:
+            model = SimpleLinearRegression(x, y)
+            try:
+                start_time = time.perf_counter()
+                model.fit(method=method)
+                duration = time.perf_counter() - start_time
+                formatted_time = self._format_duration(duration)
 
-            for method in methods:
-                model = SimpleLinearRegression(x, y)
-                try:
-                    start_time = time.perf_counter()
-                    model.fit(method=method)
-                    duration = time.perf_counter() - start_time
-                    formatted_time = self._format_duration(duration)
+                y_hat = model.predict()
 
-                    y_hat = model.predict()
+                """
+                # Performance metrics
+                # RMSE: Root Mean Squared Error - Square root of the average of the
+                #   squared differences between the predicted values and the actual
+                # MAE: Mean Absolute Error - Average absolute difference between the
+                #   predicted values and the actual
+                # R_squared:
+                """
+                self.rmse = self._calculate_rmse(y, y_hat)
+                self.mae = self._calculate_mae(y, y_hat)
+                self.r_squared = self._calculate_r_squared(y, y_hat)
 
-                    # Performance metrics
-                    # RMSE: Root Mean Squared Error - Square root of the average of the squared differences between the predicted values and the actual
-                    # MAE: Mean Absolute Error - Average absolute difference between the predicted values and the actual
-                    # R_squared:
-                    self.rmse = self._calculate_rmse(y, y_hat)
-                    self.mae = self._calculate_mae(y, y_hat)
-                    self.r_squared = self._calculate_r_squared(y, y_hat)
+                # MLflow logging
+                # with start_run(run_name=f"{method}_{n}_samples"):
+                #     log_params({"method": method, "n_samples": n})
+                #     log_metrics({
+                # "rmse": self.rmse,
+                # "mae": self.mae,
+                # "r_squared": self.r_squared,
+                # "duration_seconds": duration,
+                # "duration_pretty": formatted_time,
+                #     })
 
-                    # MLflow logging
-                    # with start_run(run_name=f"{method}_{n}_samples"):
-                    #     log_params({"method": method, "n_samples": n})
-                    #     log_metrics({
-                            # "rmse": self.rmse,
-                            # "mae": self.mae,
-                            # "r_squared": self.r_squared,
-                            # "duration_seconds": duration,
-                            # "duration_pretty": formatted_time,
-                    #     })
-
-                    coeff_results.append({
+                coeff_results.append(
+                    {
                         "n_samples": n,
                         "method": method,
                         "rmse": self.rmse,
@@ -143,25 +152,32 @@ class SimpleLinearRegression():
                         "beta_1": model.beta_1_hat,
                         "duration_seconds": duration,
                         "duration_pretty": formatted_time,
-                    })
+                    }
+                )
 
-                except Exception as e:
-                    print(f"[!] Method {method} failed at n={n}: {str(e)}")
+            except Exception as e:
+                print(f"[!] Method {method} failed at n={n}: {str(e)}")
 
-
-            return coeff_results
+        return coeff_results
 
     def simulate(
-        self, data: pd.DataFrame = None, n_samples_list: list = [10, 100, 1000], noise: float = 1.0, seed: int = 42, methods=None
+        self,
+        data: pd.DataFrame = None,
+        n_samples_list: list = [10, 100, 1000],
+        noise: float = 1.0,
+        seed: int = 42,
+        methods=None,
     ) -> pd.DataFrame:
 
         results = []
 
-        if data is not None and 'x' in data and 'y' in data:
-            x = data['x']
-            y = data['y']
+        if data is not None and "x" in data and "y" in data:
+            x = data["x"]
+            y = data["y"]
 
-            results.extend(self._coefficient_estimators(x, y, n=x.size, methods=methods))
+            results.extend(
+                self._coefficient_estimators(x, y, n=x.size, methods=methods)
+            )
 
         else:
             np.random.seed(seed)
@@ -172,7 +188,9 @@ class SimpleLinearRegression():
 
                 results.extend(self._coefficient_estimators(x, y, n, methods))
 
-        return pd.DataFrame(results).sort_values(['n_samples', 'method', 'duration_seconds'])
+        return pd.DataFrame(results).sort_values(
+            ["n_samples", "method", "duration_seconds"]
+        )
 
     def summary(self):
         print(f"Method: {self.method}")
@@ -186,10 +204,9 @@ class SimpleLinearRegression():
 
 
 if __name__ == "__main__":
-    model = SimpleLinearRegression(pd.Series([1,2,3]), pd.Series([1,2,3]))
+    model = SimpleLinearRegression(pd.Series([1, 2, 3]), pd.Series([1, 2, 3]))
 
     model.benchmark_summary()
     print()
-    model.fit('beta_estimations')
+    model.fit("beta_estimations")
     model.summary()
-
--- shared_utils/mlflow_logger.py
+++ shared_utils/mlflow_logger.py
@@ -1,13 +1,17 @@
 import mlflow
 
+
 def start_run(run_name, tags=None):
     return mlflow.start_run(run_name=run_name, tags=tags)
+
 
 def log_params(params):
     mlflow.log_params(params)
 
+
 def log_metrics(metrics):
     mlflow.log_metrics(metrics)
+
 
 def log_artifact(path):
     mlflow.log_artifact(path)
